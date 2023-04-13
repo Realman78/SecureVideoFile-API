31ae3c19cf022497f8e89e3fd6b138bde31b7df5
Realman78 <marin.dedic@me.com>
2023-04-13 23:58:13 +0200

    Added update user endpoint

--- routes/authRouter.ts
+++ routes/authRouter.ts
@@ -6,7 +6,7 @@ import Joi from 'joi'
 import ejv from 'express-joi-validation'
 const validator = ejv.createValidator({})
 
-import { postRegister, errorHandler, postLogin, getUser, verifyUser, resendConfirmationMail, resetPassword } from '../controllers/auth/authController'
+import { postRegister, errorHandler, postLogin, getUser, verifyUser, resendConfirmationMail, resetPassword, updateUser } from '../controllers/auth/authController'
 import { protect } from '../middleware/authMiddleware'
 
 const registerSchema = Joi.object({
@@ -18,12 +18,16 @@ const loginSchema = Joi.object({
     mail: Joi.string().required(),
     password: Joi.string().min(8).max(48).required(),
 })
+const updateSchema = Joi.object({
+    username: Joi.string().min(3).max(24).required(),
+})
 
 router.get('/get/me', protect, getUser)
 router.post('/register', validator.body(registerSchema), postRegister)
 router.post('/login', validator.body(loginSchema), postLogin)
 router.get('/confirm/:code', verifyUser)
 router.get('/confirm/resend/:id', resendConfirmationMail)
+router.patch('/update', validator.body(updateSchema), protect, updateUser)
 router.patch('/reset', protect, resetPassword)
 
 
