21f9c0301ab12c5ece9996323f27fa4ee84e3555
Realman78 <marin.dedic@me.com>
2023-04-16 13:40:02 +0200

    Files are deleted with user

--- controllers/auth/deleteUser.ts
+++ controllers/auth/deleteUser.ts
@@ -3,18 +3,25 @@ import File from '../../models/FileSchema'
 import { Request, Response } from 'express'
 
 const deleteUser = async (req: Request<{}, {}, {}, {}>, res: Response) => {
-    const userId = req.user.userId
-    const user = await User.findByIdAndDelete(userId)
+    try {
+        const userId = req.user.userId
+        const user = await User.findByIdAndDelete(userId)
 
-    if (!user) {
-        return res.status(404).send({ msg: "User not found." })
+        if (!user) {
+            return res.status(404).send({ msg: "User not found." })
+        }
+
+        const userFiles = await File.find({ owner: userId })
+
+        await File.deleteMany({
+            owner: userId
+        })
+
+        return res.status(200).send({ deletedUser: user, userFiles })
+    } catch (e: any) {
+        console.log(e)
+        return res.status(500).send({ message: "Error occured. Please try again.", error: e.message })
     }
-
-    const userFiles = await File.find({
-        owner: userId
-    })
-
-    return res.send({ deletedUser: user, userFiles })
 }
 
 export default deleteUser
